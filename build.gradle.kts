646387b74a27d5b4a106a0e60383c9ef12c7296a
weiluo <luow55666@gmail.com>
2026-02-24 19:57:10 -0800

    feat: Add Spring Boot 3 WebFlux skeleton

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.2"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.weiluo"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/java/com/weiluo/marketalert/MarketAlertApplication.java
@@ -0,0 +1,13 @@
+package com.weiluo.marketalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketAlertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketAlertApplication.class, args);
+    }
+
+}
